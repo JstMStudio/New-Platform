8bea5a04341343411c25b1ae03a670f69953223a
Jst Tan <jestonjst@gmail.com>
2022-10-25 18:24:39 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Change this to your repository name
+var GHPATH = '/New-Platform';
+ 
+// Choose a different app prefix name
+var APP_PREFIX = 'jstmstudio';
+ 
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
+ 
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${English}/`,
+  `${English}/index.html`,
+  `${GHPATH}/home.html`
+]
