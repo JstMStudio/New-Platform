47f95ad18fcb9a8c9f798e66206ee894bc8978be
Jst Tan <jestonjst@gmail.com>
2022-11-26 16:31:17 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,12 +8,12 @@ var APP_PREFIX = 'jstmstudio';
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_00';
+var VERSION = 'version_569a5334c2da805a323ceb937c062d6e17e68567';
  
 // The files to make available for offline use. make sure to add 
 // others to this list
 var URLS = [    
   `${English}/`,
   `${English}/index.html`,
-  `${GHPATH}/home.html`
+  `${English}/home.html`
 ]
